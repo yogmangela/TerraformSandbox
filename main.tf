1437df2731be302e3873fcc56cb304ab1b634c24
Yogesh Mangela <yogmangela@hotmail.co.uk>
2020-03-24 14:59:25 +0000

    Example of Application Load Balancer

--- main.tf
+++ main.tf
@@ -22,11 +22,8 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-
-  tags = {
-    Name = "myInstance"
-  }
 }
+
 
 resource "aws_security_group" "sg_allow8080" {
   name = "terraform-example"
@@ -39,14 +36,119 @@ resource "aws_security_group" "sg_allow8080" {
   }
 }
 
-/*
-provider "aws" {
-  region = "eu-west-1"
-  version = "~> 2.54"
+resource "aws_launch_configuration" "example" {
+      image_id= "ami-0c55b159cbfafe1f0"
+      instance_type="t2.micro"
+      security_groups=[aws_security_group.sg_allow8080.id]
+
+      user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello , World by busybox"> index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+      lifecycle{
+        create_before_destroy=true
+      }
+
 }
 
-resource "aws_instance" "mySandbox" {
-  ami=  "ami-0eda57fdc7da72118"
-  instance_type="t2.micro"
+resource "aws_autoscaling_group" "example" {
+      launch_configuration=aws_launch_configuration.example.name
+      vpc_zone_identifier=data.aws_subnet_ids.default.ids
+
+      target_group_arns=[aws_lb_target_group.asg.arn]
+      health_check_type="ELB"
+
+      min_size=2
+      max_size=10
+
+      tag{
+          key   ="Name"
+          value ="terraform-asg-example"
+          propagate_at_launch=true
+      }  
 }
-*/
+
+data "aws_vpc" "default"{
+  default=true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id=data.aws_vpc.default.id
+}
+
+resource "aws_lb" "example" {
+  name="terraform-asg-example"
+  load_balancer_type="application"
+  subnets= data.aws_subnet_ids.default.ids
+  security_groups=[aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn=aws_lb.example.arn
+  port=80
+  protocol="HTTP"
+  default_action{
+    type="fixed-response"
+    fixed_response{
+      content_type="text/plain"
+      message_body="404: page not found"
+      status_code=404
+    }
+  }
+}
+
+# create security group for ALB
+
+resource "aws_security_group" "alb" {
+  name="terraform-example-alb"
+
+  #allow inbound http reques
+  ingress{
+    from_port=80
+    to_port=80
+    protocol="tcp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
+  #allow outbound request
+  egress{
+    from_port=0
+    to_port=0
+    protocol="-1"
+    cidr_blocks=["0.0.0.0/0"]
+  } 
+}
+
+# create TG
+resource "aws_lb_target_group" "asg" {
+  name="terraform-asg-example"
+  port=var.server_port
+  protocol="HTTP"
+  vpc_id=data.aws_vpc.default.id
+
+  health_check{
+    path ="/"
+    protocol="HTTP"
+    matcher="200"
+    interval=15
+    timeout=3
+    healthy_threshold=2
+    unhealthy_threshold=2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn=aws_lb_listener.http.arn
+  priority=100
+
+  condition{
+    field="path-pattern"
+    values=["*"]
+  }
+  action{
+    type ="forward"
+    target_group_arn=aws_lb_target_group.asg.arn
+  }
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
 output "public_ip" {
   value = aws_instance.example.public_ip
   #value= concat((aws_instance.example.public_ip),(var.server_port))
-
 }
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description="the domain of the LB"
+}
+
